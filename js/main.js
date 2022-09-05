1cb928c215d78aa9accb9c12cf4d41bbc36e1c2d
EstebanPechuan <esteban.pechuan@gmail.com>
2022-09-04 23:23:32 -0300

    Entrega proyecto

---
+++ js/main.js
@@ -0,0 +1,29 @@
+let respuesta = window.fetch('./js/data.json')
+    .then(res => res.json())
+    .then(datos => imprimirDatos(datos))
+
+
+function imprimirDatos(datos) {
+    const graficos = document.querySelector('.container__main__graph')
+    datos.map(item => {
+        let div = `
+            <div class="container__day">
+            <div class="container__day__graph" onclick="graphActive(this)" style="height: ${item.amount * 1.2}%">
+                <div class="container__day__graph--hover">$${item.amount}</div>
+            </div>
+                ${item.day}
+            </div>
+        `
+        graficos.innerHTML += div
+    })
+}
+
+function graphActive(el) {
+    const days = document.querySelectorAll('.container__day__graph')
+
+    days.forEach(item => {
+        item.classList.remove('container__day__graph--active')
+    });
+
+    el.classList.add('container__day__graph--active')
+}
